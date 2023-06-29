424acb2f047b12024c5d74a7d9d48a02d4568547
Lucas Balieiro <lucas.matos@indt.org.br>
2023-06-29 11:04:21 -0400

    fix comments typo

--- test/test_add.py
+++ test/test_add.py
@@ -1,24 +1,23 @@
 import demo
 import pytest
 
-#agrupamos por classes para deixar mais claro a especificidade de cada teste
+# Agrupando por classes para deixar mais claro a especificidade de cada teste
 class TestAdd:
 
-    #Estrutura basica do teste
+    # Estrutura básica do teste
     def test(self):
-        #Arrange: configuramos os parametros necessarios para o teste
+        # Arrange: Configurando os parâmetros necessários para o teste
         calculator = demo.Calculator()
 
-        #Execute : Executamos o que precisa ser testado
-        result = calculator.add(2,2)
+        # Act: Executando o que precisa ser testado
+        result = calculator.add(2, 2)
 
-        #Assert: Fazemos as verificacoes para saber se os testes passaram
+        # Assert: Verificando se o resultado está correto
         assert result == 4
 
     # Teste de Exceptions
-    def test_add_error(self,instance_calculator):
-        # neste gerenciador de contexto, fazemos o pytest esperar
-        #  o lancamento de uma exception
+    def test_add_error(self, instance_calculator):
+        # Neste bloco de código, usamos o pytest para verificar se ocorre uma exceção específica
         with pytest.raises(demo.CustomError):
             instance_calculator.add(666, 6)
 
@@ -31,26 +30,24 @@ class TestAdd:
         ]
     )
     def test_with_multiple_parameters(self, a, b, expected, instance_calculator):
-        #cada tupla configurada no parametrize eh executada
+        # Cada tupla configurada no parametrize é executada separadamente
         assert instance_calculator.add(a, b) == expected
 
     def test_output(self, capsys, instance_calculator):
-        
-        instance_calculator.add(2,2)
+        # Capturando a saída do método add usando capsys
+        instance_calculator.add(2, 2)
         
         captured = capsys.readouterr()
         
+        # Verificando se a saída está correta
         assert captured.out == f"adicao executada\n"
         assert captured.err == "sem erros"
         
     def test_with_mock(self, monkeypatch, instance_calculator):
-        def mock_add(self, a , b):
+        def mock_add(self, a, b):
             return 42
         
-        #monkeypatch subsitui o comportamento padrao de uma funcao
-        # para o mock_add
+        # monkeypatch substitui o comportamento padrão da função "add" pelo "mock_add"
         monkeypatch.setattr(demo.Calculator, "add", mock_add)
 
-        assert instance_calculator.add(2,2) == 42
-       
-
+        assert instance_calculator.add(2, 2) == 42
